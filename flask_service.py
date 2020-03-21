729e28ee3e4a9f93b3d2be960e53c59c6e2a6865
fherrmann <fherrmann@techfak.uni-bielefeld.de>
2020-03-21 20:13:40 +0100

    Added first draft of API

---
+++ flask_service.py
@@ -0,0 +1,71 @@
+import json
+from flask import Flask, request
+from flask_restful import Resource, Api, abort
+from postgre_backend import DatabaseConnector as dbc
+import uuid
+
+def create_database_connection():
+    with open("secrets.txt", 'r') as infile:
+        data = infile.read()
+    credentials = json.loads(data)
+    connector = dbc(user=credentials["user"],
+                    password=credentials["password"],
+                    host=credentials["host"],
+                    port=credentials["port"],
+                    database=credentials["database"])
+    return connector
+
+app = Flask(__name__)
+api = Api(app)
+
+
+class Student(Resource):
+    def get(self, student_uid):
+        if not student_uid:
+            abort(400, message="Parameter student_uid is empty.")
+        else:
+            query = f"SELECT student_uid, name, class FROM student WHERE student_uid = '{student_uid}';"
+        results = connector.receive_from_database(query)
+        if not results:
+            abort(404, message="Parameter student_uid does not exist.")
+        return results
+
+    def delete(self, student_uid):
+        if not student_uid:
+            abort(400, message="Parameter student_uid is empty.")
+        else:
+            query = f"DELETE FROM student WHERE student_uid = '{student_uid}';"
+        connector.write_to_database(query)
+        # TODO: add better exception handling if userid does not exist in database
+
+
+class Students(Resource):
+    def get(self):
+        query = "SELECT student_uid, name, class FROM student;"
+        results = connector.receive_from_database(query)
+        if not results:
+            abort(404, message="No student exists.")
+        return results
+
+    def post(self):
+        new_user = request.get_json()
+        if not new_user:
+            abort(400, message="Student is not valid.")
+        student_uid = uuid.uuid4()
+        query = f"INSERT INTO student (student_uid, name, class) VALUES ('{student_uid}', '{new_user['name']}', '{new_user['class']}')"
+        connector.write_to_database(query)
+        return {"student_uid": str(student_uid)}
+
+# add URLs
+api.add_resource(Student, '/student/<student_uid>')
+api.add_resource(Students, '/student')
+
+# TODO: make a redirect instead
+@app.route('/')
+def hello_world():
+    return "Hello World"
+
+
+if __name__ == '__main__':
+    connector = create_database_connection()
+    app.run(debug=True)
---
+++ postgre_backend.py
@@ -0,0 +1,59 @@
+from typing import List
+import psycopg2
+
+class DatabaseConnector:
+    def __init__(self, user, password, host, port, database):
+        self.user = user
+        self.password = password
+        self.host = host
+        self.port = port
+        self.database = database
+        self.connection = psycopg2.connect(user=self.user,
+                                           password=self.password,
+                                           host=self.host,
+                                           port=self.port,
+                                           database=self.database)
+
+    def check_connection(self):
+        if self.connection.closed == 1:
+            self.connection = psycopg2.connect(user=self.user,
+                                           password=self.password,
+                                           host=self.host,
+                                           port=self.port,
+                                           database=self.database)
+
+
+    def receive_from_database(self, query: str) -> List:
+        self.check_connection()
+        try:
+            cur = self.connection.cursor()
+            cur.execute(query)
+            records = cur.fetchall()
+            return records
+
+        except (Exception, psycopg2.Error) as error:
+            print("Error while connecting to PostgreSQL", error)
+            return []
+
+        finally:
+                if (self.connection):
+                    cur.close()
+                    self.connection.close()
+                    print("PostgreSQL connection is closed")
+
+
+    def write_to_database(self, query):
+        self.check_connection()
+        try:
+            cur = self.connection.cursor()
+            cur.execute(query)
+            self.connection.commit()
+
+        except (Exception, psycopg2.Error) as error:
+            print("Error while connecting to PostgreSQL", error)
+
+        finally:
+                if (self.connection):
+                    cur.close()
+                    self.connection.close()
+                    print("PostgreSQL connection is closed")
